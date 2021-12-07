938bd8d4c4551bcbb138a866545ef91771d3a5e9
JFmaia <jfmaia741@gmail.com>
2021-12-07 08:17:33 -0300

    Classe que instancia os themas de cores e estilos de texto.

---
+++ tacaro/lib/shared/theme/app_theme.dart
@@ -0,0 +1,12 @@
+import 'package:tacaro/shared/theme/app_colors.dart';
+import 'package:tacaro/shared/theme/text_styles.dart';
+
+class AppTheme {
+  static final AppTheme instance = AppTheme();
+
+  final _colors = AppColors();
+  static AppColors get colors => instance._colors;
+
+  final _textStyles = AppTextStyles();
+  static AppTextStyles get textStyles => instance._textStyles;
+}
