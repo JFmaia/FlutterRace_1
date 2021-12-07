00d845f067423b69bd20e6f953279371edf817d2
JFmaia <jfmaia741@gmail.com>
2021-12-07 11:36:00 -0300

    Pasta de login.

---
+++ tacaro/lib/modules/login/login_page.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:tacaro/shared/theme/app_theme.dart';
+import 'package:tacaro/shared/widgets/button/button.dart';
+import 'package:tacaro/shared/widgets/input_text/input_text.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: AppTheme.colors.background,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 50,
+              horizontal: 40,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                  child: SizedBox(
+                    height: size.height * 0.32,
+                    width: size.width * 0.60,
+                    child: Image.asset(
+                      "assets/images/logo.png",
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                InputText(
+                  label: "Email",
+                  hint: "contato@fluterando.com",
+                ),
+                SizedBox(height: 18),
+                InputText(
+                  label: "Senha",
+                  hint: "Digite sua senha",
+                ),
+                SizedBox(height: 15),
+                Button(
+                  size: size,
+                  label: "Entrar",
+                  type: ButtonType.fill,
+                  onPressed: () {},
+                ),
+                SizedBox(height: 30),
+                Button(
+                  size: size,
+                  label: "Criar conta",
+                  type: ButtonType.outline,
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
