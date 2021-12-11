13436f2d65019e3249f5974c3ad29c1545ef09bd
JFmaia <jfmaia741@gmail.com>
2021-12-11 18:48:21 -0300

    Modificando AppDataBase.
    Criando novas contratos.

--- tacaro/lib/shared/services/app_database.dart
+++ tacaro/lib/shared/services/app_database.dart
@@ -1,10 +1,13 @@
+import 'package:tacaro/modules/models/user_model.dart';
 import 'package:tacaro/shared/services/supabase_database.dart';
 
 abstract class AppDataBase {
   static final AppDataBase instance = SupabaseDataBase();
   void init();
-  Future<bool> login({required String email, required String password});
-  Future<bool> createAccount({
+  Future<UserModel> createUser(UserModel user);
+  Future<UserModel> getUser(String id);
+  Future<UserModel> login({required String email, required String password});
+  Future<UserModel> createAccount({
     required String email,
     required String password,
     required String name,
