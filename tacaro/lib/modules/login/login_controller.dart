436f2becb63127691005eaad5b77725e465b115b
JFmaia <jfmaia741@gmail.com>
2021-12-11 15:19:58 -0300

    Adicionando a chamada de criar conta..
    Fazendo cadastro usando AppdataBase.

--- tacaro/lib/modules/login/login_controller.dart
+++ tacaro/lib/modules/login/login_controller.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tacaro/shared/services/app_database.dart';
 import 'package:tacaro/shared/utils/app_state.dart';
 
 class LoginController extends ChangeNotifier {
@@ -31,7 +32,7 @@ class LoginController extends ChangeNotifier {
     if (validate()) {
       try {
         update(AppState.loading());
-        await Future.delayed(Duration(seconds: 4));
+        await AppDataBase.instance.login(email: _email, password: _password);
         update(AppState.success<String>("Usuario logado"));
       } catch (e) {
         update(AppState.error("Não foi possível realizar login!"));
