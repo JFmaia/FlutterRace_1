e839df5dacac33be7902b91ec82183cdd6579284
JFmaia <jfmaia741@gmail.com>
2021-12-11 22:31:46 -0300

    Crinado widget de Card_product.

---
+++ tacaro/lib/shared/widgets/card_product/card_product.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:tacaro/shared/theme/app_theme.dart';
+
+class CardProduct extends StatelessWidget {
+  const CardProduct({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        width: 200,
+        decoration: BoxDecoration(
+            color: AppTheme.colors.textEnabled,
+            borderRadius: BorderRadius.circular(12)),
+        child: Column(
+          children: [
+            ListTile(
+              leading: CircleAvatar(
+                backgroundColor: AppTheme.colors.background,
+                radius: 30,
+                child: Icon(Icons.library_add),
+              ),
+              title: Text("Produto"),
+              subtitle: Text("Preço"),
+            ),
+            Text.rich(
+              TextSpan(
+                text: "Agora\n",
+                children: const [
+                  TextSpan(text: "R\$ 67.50"),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
