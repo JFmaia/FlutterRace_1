89939ebb04d4f7a70ca679160d5a6941933a0623
JFmaia <jfmaia741@gmail.com>
2021-12-13 10:40:50 -0300

    Criando PageRoutes.

--- tacaro/lib/app_widget.dart
+++ tacaro/lib/app_widget.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:tacaro/modules/feed/feed_page.dart';
 import 'package:tacaro/modules/home/home_page.dart';
 import 'package:tacaro/modules/login/login_page.dart';
 import 'package:tacaro/modules/login/pages/create_account/create_account_page.dart';
+import 'package:tacaro/modules/profile/profile_page.dart';
 import 'package:tacaro/modules/splash/splash.dart';
 import 'package:tacaro/shared/models/user_model.dart';
 
@@ -11,16 +13,17 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: "Flutter Race #1",
       theme: ThemeData(primarySwatch: Colors.green),
-      initialRoute: "/home",
+      initialRoute: "/splash",
       routes: {
         "/splash": (context) => const SplashPage(),
         "/login": (context) => const LoginPage(),
         "/login/create-account": (context) => const CreateAccountPage(),
         "/home": (context) => HomePage(
-            user: ModalRoute.of(context)!.settings.arguments as UserModel),
+              pages: const [FeedPage(), ProfilePage()],
+              user: ModalRoute.of(context)!.settings.arguments as UserModel,
+            )
       },
     );
   }
