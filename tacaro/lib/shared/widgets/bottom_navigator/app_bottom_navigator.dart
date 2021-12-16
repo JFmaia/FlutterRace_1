fb436e9769766b7d2aa056f1c4f410d18f67b8fb
JFmaia <jfmaia741@gmail.com>
2021-12-16 09:19:49 -0300

    Modificando formato do BottomNavigator.

--- tacaro/lib/shared/widgets/bottom_navigator/app_bottom_navigator.dart
+++ tacaro/lib/shared/widgets/bottom_navigator/app_bottom_navigator.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tacaro/shared/theme/app_theme.dart';
 import 'package:tacaro/shared/widgets/icon_buttom_navigator/icon_buttom_navigator.dart';
 
 class AppBottomNavigator extends StatelessWidget {
@@ -12,43 +13,36 @@ class AppBottomNavigator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(
-        right: 26,
-        left: 26,
+    return Container(
+      height: 60,
+      decoration: BoxDecoration(
+        color: AppTheme.colors.textEnabled,
       ),
-      child: Container(
-        height: 60,
-        decoration: BoxDecoration(
-          color: Colors.transparent,
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            IconBottomNavigator(
-              icon: Icons.home,
-              onTap: () {
-                onChanged(0);
-              },
-              enabled: currentIndex == 0,
-            ),
-            IconBottomNavigator(
-              icon: Icons.add,
-              onTap: () {
-                onChanged(3);
-              },
-              enabled: false,
-            ),
-            IconBottomNavigator(
-              icon: Icons.settings,
-              onTap: () {
-                onChanged(1);
-              },
-              enabled: currentIndex == 1,
-            ),
-          ],
-        ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          IconBottomNavigator(
+            icon: Icons.home,
+            onTap: () {
+              onChanged(0);
+            },
+            enabled: currentIndex == 0,
+          ),
+          IconBottomNavigator(
+            icon: Icons.add,
+            onTap: () {
+              onChanged(3);
+            },
+            enabled: false,
+          ),
+          IconBottomNavigator(
+            icon: Icons.settings,
+            onTap: () {
+              onChanged(1);
+            },
+            enabled: currentIndex == 1,
+          ),
+        ],
       ),
     );
   }
