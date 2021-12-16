0478a63b47ab3b98d208964a04a8fa824ff38d88
JFmaia <jfmaia741@gmail.com>
2021-12-16 09:30:39 -0300

    Ajustando scroll do bottomshet.
    Ao chamar o teclado agora o bottomshe move para cima, dando a oportunidade e observar o que vc estar digitando.

--- tacaro/lib/modules/create/create_bottomsheet.dart
+++ tacaro/lib/modules/create/create_bottomsheet.dart
@@ -38,72 +38,77 @@ class _CreateBottomsheetState extends State<CreateBottomsheet> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(
-        horizontal: 42,
+    return Padding(
+      padding: EdgeInsets.only(
+        bottom: MediaQuery.of(context).viewInsets.bottom,
       ),
-      child: Form(
-        key: controller.formKey,
-        child: Column(
-          children: [
-            SizedBox(
-              height: 32,
-            ),
-            InputText(
-              label: "Produto",
-              hint: "Digite um nome",
-              onChanged: (value) => controller.onChange(name: value),
-              validator: (value) =>
-                  value.isNotEmpty ? null : "Favor digitar um nome",
-            ),
-            SizedBox(
-              height: 8,
-            ),
-            InputText(
-              label: "Preço",
-              hint: "Digite o valor",
-              onChanged: (value) => controller.onChange(price: value),
-              keyboardType: TextInputType.number,
-              validator: (value) =>
-                  value.isNotEmpty ? null : "Favor digitar um valor",
-              inputFormatters: [
-                MoneyInputFormatter(
-                  leadingSymbol: "R\$",
-                )
-              ],
-            ),
-            SizedBox(
-              height: 8,
-            ),
-            InputText(
-                keyboardType: TextInputType.datetime,
-                onChanged: (value) => controller.onChange(date: value),
-                label: "Data da compra",
-                inputFormatters: [MaskedInputFormatter('00/00/0000')],
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: 42,
+        ),
+        child: Form(
+          key: controller.formKey,
+          child: Column(
+            children: [
+              SizedBox(
+                height: 32,
+              ),
+              InputText(
+                label: "Produto",
+                hint: "Digite um nome",
+                onChanged: (value) => controller.onChange(name: value),
                 validator: (value) =>
-                    value.isNotEmpty ? null : "Favor digitar uma data",
-                hint: "Digite dd/mm/aa"),
-            SizedBox(
-              height: 8,
-            ),
-            SizedBox(
-              height: 4,
-            ),
-            AnimatedBuilder(
-              animation: controller,
-              builder: (_, __) => controller.state.when(
-                loading: () => CircularProgressIndicator(),
-                error: (message, _) => Text(message),
-                orElse: () => Button(
-                  label: "Adicionar",
-                  onPressed: () {
-                    controller.create();
-                  },
-                  size: MediaQuery.of(context).size * 0.90,
+                    value.isNotEmpty ? null : "Favor digitar um nome",
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              InputText(
+                label: "Preço",
+                hint: "Digite o valor",
+                onChanged: (value) => controller.onChange(price: value),
+                keyboardType: TextInputType.number,
+                validator: (value) =>
+                    value.isNotEmpty ? null : "Favor digitar um valor",
+                inputFormatters: [
+                  MoneyInputFormatter(
+                    leadingSymbol: "R\$",
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              InputText(
+                  keyboardType: TextInputType.datetime,
+                  onChanged: (value) => controller.onChange(date: value),
+                  label: "Data da compra",
+                  inputFormatters: [MaskedInputFormatter('00/00/0000')],
+                  validator: (value) =>
+                      value.isNotEmpty ? null : "Favor digitar uma data",
+                  hint: "Digite dd/mm/aa"),
+              SizedBox(
+                height: 8,
+              ),
+              SizedBox(
+                height: 4,
+              ),
+              AnimatedBuilder(
+                animation: controller,
+                builder: (_, __) => controller.state.when(
+                  loading: () => CircularProgressIndicator(),
+                  error: (message, _) => Text(message),
+                  orElse: () => Button(
+                    label: "Adicionar",
+                    onPressed: () {
+                      controller.create();
+                    },
+                    size: MediaQuery.of(context).size * 0.90,
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
